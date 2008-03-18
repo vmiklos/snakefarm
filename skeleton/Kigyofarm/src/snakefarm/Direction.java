c1242887255c605c392060ac7e7c642458750fc8
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-03-18 00:08:28 +0000

    Direction class dokumentalasa
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@71 dfce5827-3746-0410-9ec7-c6586dabde71

--- skeleton/Kigyofarm/src/snakefarm/Direction.java
+++ skeleton/Kigyofarm/src/snakefarm/Direction.java
@@ -1,19 +1,27 @@
 package snakefarm;
-/*
- * 0 - right
- * 1 - up
- * 2 - left
- * 3 - down
- * */
 
+/**
+ * Ez a class valositja meg a kigyonak egy haladasi iranyat.
+ */
 public class Direction {
 
 	private static int lastid = 0;
 	private int id = lastid;
 	private static final String type = "Direction";
+	/**
+	 * Megadja, hogy maximum hany iranyban allhat a kigyo.
+	 */
 	public static final int numberOfDirections = 4;
+	/**
+	 * Az iranyt tarolja: 0 - jobb, 1 - fel, 2 - balra, 3 - le.
+	 */
 	private int dir;
 
+	/**
+	 * Beallitja az iranyt.
+	 *
+	 * @param dir az uj irany
+	 */
 	public Direction(int dir) {
 		lastid++;
 		Skeleton.enterMethod(type, id, "Direction(int)");
@@ -21,6 +29,9 @@ public class Direction {
 		Skeleton.exitMethod(type, id, "Direction(int)");
 	}
 
+	/**
+	 * Az irany osztaly konstruktora.
+	 */
 	public Direction() {
 		lastid++;
 		Skeleton.enterMethod(type, id, "Direction()");
@@ -28,11 +39,19 @@ public class Direction {
 		Skeleton.exitMethod(type, id, "Direction()");
 	}
 
+	/**
+	 * Visszaad egy hash kodot az objektumra.
+	 */
 	@Override
 	public int hashCode() {
 		return dir;
 	}
 
+	/**
+	 * Osszehasonlitja az objektumot egy masik irannyal.
+	 *
+	 * @return az osszeshasonlitas eredmenye
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (obj == null) {
@@ -48,18 +67,27 @@ public class Direction {
 		return true;
 	}
 
+	/**
+	 * Balra forditja az iranyt.
+	 */
 	public void turnLeft() {
 		Skeleton.enterMethod(type, id, "turnLeft()");
 		dir = (dir + 1) % numberOfDirections;
 		Skeleton.exitMethod(type, id, "turnLeft()");
 	}
 
+	/**
+	 * Jobbra forditja az iranyt.
+	 */
 	public void turnRight() {
 		Skeleton.enterMethod(type, id, "turnRight()");
 		dir = (dir + numberOfDirections - 1) % numberOfDirections;
 		Skeleton.exitMethod(type, id, "turnRight()");
 	}
 
+	/**
+	 * Az ellenkezojere forditja az iranyt.
+	 */
 	public Direction reverse() {
 		Skeleton.enterMethod(type, id, "reverse()");
 		Skeleton.exitMethod(type, id, "reverse()");
