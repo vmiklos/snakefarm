e556977afbd15058a39476a2bf3cf0df3efa161d
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-03-18 00:08:05 +0000

    Collidable abstract class dokumentalasa
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@69 dfce5827-3746-0410-9ec7-c6586dabde71

--- skeleton/Kigyofarm/src/snakefarm/Collidable.java
+++ skeleton/Kigyofarm/src/snakefarm/Collidable.java
@@ -1,20 +1,37 @@
 package snakefarm;
 
+/**
+ * Vilagobjektum. Ebbol a classbol oroklodik az osszes mezon
+ * megtalalhato elem, amelyek kepes utkozni.
+ */
 public abstract class Collidable {
 
+	/**
+	 * Ez a referencia arra a mezore mutat amin jelenleg allunk.
+	 */
 	protected Field field;
 
+	/**
+	 * Utkozik a SnakeUnit kigyo egyseggel es visszater az utkozes
+	 * utan maradt vilagobjektummal.
+	 *
+	 * @param utkozo kigyoelem
+	 */
 	public abstract void collideWith(SnakeUnit snakeUnit);
 
-	/* Ezt leirtam az uml diagramban is, de ide is leirom
-	 * Alapertelmezetten a collideWithSaw fv. ugyanaz mint a collideWith, csak a SnakeUnitnal kell mas
-	 * Ez a fv. hivodik meg akkor, amikor fureszfejes kigyo utkozik az objektumba
-	 * Tehat defaultbol csak meghivja a colldeWith() fv.-t ugyanazzal a parameterrel
-	 * Ezt sztem ide kene irni, tekintve, hogy kozos osztalyrol van szo es nem kozos interfeszrol*/
+	/**
+	 * Alapertelmezettel ugyanaz, mint a collideWith(), de a
+	 * SnakeUnitnal override-olja.
+	 */
 	public void collideWithSaw(SnakeUnit snakeUnit) {
 		collideWith(snakeUnit);
 	}
 
+	/**
+	 * Beallitja, hogy melyik mezon vagyunk.
+	 *
+	 * @param field az uj mezo referenciaja
+	 */
 	public void setField(Field field) {
 		this.field = field;
 	}
