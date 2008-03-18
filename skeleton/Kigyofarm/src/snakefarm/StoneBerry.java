3b5281012dac5bd218a73bc79a74cac96b51dc25
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-03-18 01:31:32 +0000

    StoneBerry doksi
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@82 dfce5827-3746-0410-9ec7-c6586dabde71

--- skeleton/Kigyofarm/src/snakefarm/StoneBerry.java
+++ skeleton/Kigyofarm/src/snakefarm/StoneBerry.java
@@ -1,11 +1,19 @@
 package snakefarm;
 
+/**
+ * Kobogyo osztaly.
+ */
 public class StoneBerry extends Collidable {
 
 	private static int lastid = 0;
 	private int id = lastid;
 	private static final String type = "StoneBerry";
 
+	/**
+	 * Kobogyo konstruktora.
+	 *
+	 * @param field melyik mezon jojjon letre
+	 */
 	public StoneBerry(Field field) {
 		lastid++;
 		Skeleton.enterMethod(type, id, "StoneBerry()");
@@ -14,6 +22,11 @@ public class StoneBerry extends Collidable {
 		Skeleton.exitMethod(type, id, "StoneBerry()");
 	}
 
+	/**
+	 * Utkozes kigyoelemmel
+	 *
+	 * @param snakeUnit utkozo kigyoelem
+	 */
 	public void collideWith(SnakeUnit snakeUnit) {
 		Skeleton.enterMethod(type, id, "collideWith(SnakeUnit)");
 		snakeUnit.collideWith2(this);
