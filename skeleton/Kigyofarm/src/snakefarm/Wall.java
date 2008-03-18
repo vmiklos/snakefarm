29e9432ebf7b8b9bd074e1e1aa5fa434dcd7ebd9
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-03-18 01:31:44 +0000

    Wall dokumentalasa
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@83 dfce5827-3746-0410-9ec7-c6586dabde71

--- skeleton/Kigyofarm/src/snakefarm/Wall.java
+++ skeleton/Kigyofarm/src/snakefarm/Wall.java
@@ -1,11 +1,21 @@
 package snakefarm;
 
+/**
+ * Fal objektum.
+ * <p>
+ * Ez veszi körül a pályát. Ha belemegy a kígyó, meghal.
+ */
 public class Wall extends Collidable {
 
 	private static int lastid = 0;
 	private int id = lastid;
 	private static final String type = "Wall";
 
+	/**
+	 * Fal konstruktora.
+	 *
+	 * @param field melyik mezon jojjon letre
+	 */
 	public Wall(Field field) {
 		lastid++;
 		Skeleton.enterMethod(type, id, "Wall()");
@@ -14,14 +24,26 @@ public class Wall extends Collidable {
 		Skeleton.exitMethod(type, id, "Wall()");
 	}
 
+	/**
+	 * Utkozes kigyoelemmel.
+	 *
+	 * @param snakeUnit utkozo kigyoelem
+	 */
 	public void collideWith(SnakeUnit snakeUnit) {
 		Skeleton.enterMethod(type, id, "collideWith(SnakeUnit)");
 		snakeUnit.collideWith2(this);
 		Skeleton.exitMethod(type, id, "collideWith(SnakeUnit)");
 	}
 
+	/*
+	 * Utkozes fureszmodban levo kigyoelemmel.
+	 *
+	 * @param snakeUnit utkozo kigyoelem
+	 */
 	@Override
 	public void collideWithSaw(SnakeUnit snakeUnit) {
+		/* TODO: wtf? nem tokmind1, hha falnak megyunk akkor
+		 * van-e fureszunk? (vmiklos) */
 		Skeleton.enterMethod(type, id, "collideWith(SnakeUnit)");
 		snakeUnit.collideWith2(this);
 		Skeleton.exitMethod(type, id, "collideWith(SnakeUnit)");
