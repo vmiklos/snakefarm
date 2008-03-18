c9f17fabd9a13e8817836dbbb4e4b996258c0d10
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-03-18 00:08:17 +0000

    Skeletonhoz meg doksi
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@70 dfce5827-3746-0410-9ec7-c6586dabde71

--- skeleton/Kigyofarm/src/snakefarm/Skeleton.java
+++ skeleton/Kigyofarm/src/snakefarm/Skeleton.java
@@ -5,6 +5,10 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+/**
+ * Ez a skeleton fo classa, innen indul a program. Soha nem
+ * peldanyositjuk.
+ */
 public class Skeleton {
 
 	private static int indentation = 0;
