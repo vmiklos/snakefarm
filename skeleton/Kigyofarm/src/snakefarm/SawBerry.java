c23de4aed12e3081c3391fca38568cbb8fe701e6
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-03-18 01:09:42 +0000

    SawBerry doksi
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@77 dfce5827-3746-0410-9ec7-c6586dabde71

--- skeleton/Kigyofarm/src/snakefarm/SawBerry.java
+++ skeleton/Kigyofarm/src/snakefarm/SawBerry.java
@@ -1,11 +1,18 @@
 package snakefarm;
 
+/**
+ * Fureszbogyo objektum. Csupan a Collidable classbol szarmazo
+ * utkozes fuggvenyt kell megvalositani a helyes mukodesehez.
+ */
 public class SawBerry extends Collidable {
 
 	private static int lastid = 0;
 	private int id = lastid;
 	private static final String type = "SawBerry";
 
+	/**
+	 * A fureszbogyo konstruktora.
+	 */
 	public SawBerry(Field field) {
 		lastid++;
 		Skeleton.enterMethod(type, id, "SawBerry()");
@@ -14,6 +21,9 @@ public class SawBerry extends Collidable {
 		Skeleton.exitMethod(type, id, "SawBerry()");
 	}
 
+	/**
+	 * Utkoztet egy kigyoelemet a fureszbogyoval.
+	 */
 	public void collideWith(SnakeUnit snakeUnit) {
 		Skeleton.enterMethod(type, id, "collideWith(SnakeUnit)");
 		snakeUnit.collideWith2(this);
