151e0a29cb6513ce666437d60d3a5d4ba9688c0a
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-03-18 01:09:29 +0000

    Player doksi
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@76 dfce5827-3746-0410-9ec7-c6586dabde71

--- skeleton/Kigyofarm/src/snakefarm/Player.java
+++ skeleton/Kigyofarm/src/snakefarm/Player.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.Iterator;
 
+/**
+ * Jatekos alaposztaly. Tartalmazza a jatekos kigyoit. Felel a sajat
+ * kigyoinak a letrehozasaert.
+ */
 public class Player {
 
 	private static int lastid = 0;
@@ -13,6 +17,11 @@ public class Player {
 	private List<Snake> snakes = new LinkedList<Snake>();
 	private List<Snake> temp = new LinkedList<Snake>();
 
+	/**
+	 * A jatekos osztaly konstruktora.
+	 *
+	 * @param game a jatek amelyben a jatekos letrejon
+	 */
 	public Player(Game game) {
 		lastid++;
 		Skeleton.enterMethod(type, id, "Player(Game)");
@@ -20,12 +29,26 @@ public class Player {
 		Skeleton.exitMethod(type, id, "Player(Game)");
 	}
 
+	/**
+	 * Megmondja, hogy egy mezorol, hogy a rajta levo kigyo a
+	 * jatekoshoz tartozik.
+	 *
+	 * @param field a mezo
+	 */
 	public void addSnake(Field field) {
+		/* FIXME: ez baromsag, ha egyszer mezot adunk meg akkor
+		 * miert addSnake.. */
 		Skeleton.enterMethod(type, id, "addSnake(Field)");
 		snakes.add(new Snake(this, field));
 		Skeleton.exitMethod(type, id, "addSnake(Field)");
 	}
 
+	/**
+	 * Lepteti a jatekost.
+	 * <p>
+	 * Ez konkretan azt jelenti, hogy a jatekos minden kigyojat
+	 * lepteti.
+	 */
 	public void step() {
 		Skeleton.enterMethod(type, id, "step()");
 		temp.addAll(snakes);
@@ -36,6 +59,9 @@ public class Player {
 		Skeleton.exitMethod(type, id, "step()");
 	}
 
+	/**
+	 * Eltavolit egy kigyot a jatekostol.
+	 */
 	public void removeSnake(Snake snake) {
 		Skeleton.enterMethod(type, id, "removeSnake(Snake)");
 		snakes.remove(snake);
