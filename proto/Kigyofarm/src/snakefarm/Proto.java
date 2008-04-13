e6acdba8a27afba63a254026d6b7c1e1c5a49c56
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-04-13 01:16:30 +0000

    test1 elso 3 sora bedrotozva, hogy lehessen parsert irni
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@113 dfce5827-3746-0410-9ec7-c6586dabde71

--- proto/Kigyofarm/src/snakefarm/Proto.java
+++ proto/Kigyofarm/src/snakefarm/Proto.java
@@ -3,17 +3,27 @@ package snakefarm;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintStream;
 
 /**
  * Ez a proto fo classa, innen indul a program. Soha nem
  * peldanyositjuk.
  */
 public class Proto {
+	public static PrintStream out = null;
+	private static boolean debug = false;
+	private static int time = 0;
 
 	/**
 	 * Ez a proto main fuggvenye.
 	 * @param args Commandline argumentumok.
 	 */
 	public static void main(String args[]) {
+		debug = true;
+		out = System.out;
+		time = 100;
+		out.println(debug);
+		out.println(out);
+		out.println(time);
 	}
 }
