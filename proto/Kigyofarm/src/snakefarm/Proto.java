3dcf701f836cac0151c5365b7b86982d60b85360
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-04-13 01:16:55 +0000

    uj out es time parancsok
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@115 dfce5827-3746-0410-9ec7-c6586dabde71

--- proto/Kigyofarm/src/snakefarm/Proto.java
+++ proto/Kigyofarm/src/snakefarm/Proto.java
@@ -11,14 +11,14 @@ import java.io.PrintStream;
  */
 public class Proto {
 	private Game game;
-	public static PrintStream out = System.out;
+	public static PrintStream out = null;
 	private static boolean debug = false;
 	private static int time = 0;
 
 	private class Debug implements CommandParser {
 		public void parseCommand(String[] args) throws Exception {
 			if(args.length != 1)
-				throw new Exception("Debug.parseCommand: too few parameters");
+				throw new Exception("Proto.Debug.parseCommand: too few parameters");
 			if(Integer.parseInt(args[0]) == 1)
 				debug = true;
 			else
@@ -26,19 +26,37 @@ public class Proto {
 		}
 	}
 
+	private class Out implements CommandParser {
+		public void parseCommand(String[] args) throws Exception {
+			if(args.length != 1)
+				throw new Exception("Proto.Out.parseCommand: too few parameters");
+			int target = Integer.parseInt(args[0]);
+			if(target == 1)
+				out = System.out;
+			else
+				throw new Exception("Proto.Out.parseCommand: != 1 not yet implemented");
+		}
+	}
+
+	private class Time implements CommandParser {
+		public void parseCommand(String[] args) throws Exception {
+			if(args.length != 1)
+				throw new Exception("Proto.Time.parseCommand: too few parameters");
+			time = Integer.parseInt(args[0]);
+		}
+	}
+
 	public Proto(String fileName) throws Exception {
 		game = new Game();
 		Parser parser = new Parser();
 		parser.addCommand("debug", new Debug());
-		//parser.addCommand("out", new Out());
-		//parser.addCommand("time", new Time());
+		parser.addCommand("out", new Out());
+		parser.addCommand("time", new Time());
 		parser.parse(fileName);
 
-		//out = System.out;
-		//time = 100;
 		out.println(debug);
-		//out.println(out);
-		//out.println(time);
+		out.println(out);
+		out.println(time);
 	}
 
 	/**
