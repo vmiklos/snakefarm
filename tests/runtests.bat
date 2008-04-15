a5a8196d44e9eb4c4f37cbb8cbd9c61e52e90a52
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-04-15 18:51:09 +0000

    runtests.*: 19es teszt futtatasa
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@225 dfce5827-3746-0410-9ec7-c6586dabde71

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -70,3 +70,7 @@ java -jar compare/TestCompare.jar test17.out curtest
 echo Running test 18...
 java -jar ../proto/Kigyofarm/src/Kigyofarm.jar test18.in >curtest
 java -jar compare/TestCompare.jar test18.out curtest
+
+echo Running test 19...
+java -jar ../proto/Kigyofarm/src/Kigyofarm.jar test19.in >curtest
+java -jar compare/TestCompare.jar test19.out curtest
