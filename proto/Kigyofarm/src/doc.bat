cb28c9b23b4a6fc6eb32d5e33bbe39aa894abd56
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-04-15 01:19:57 +0000

    proto/doc.bat: skeletonbol valo levels kigyomlalasa
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@211 dfce5827-3746-0410-9ec7-c6586dabde71

--- proto/Kigyofarm/src/doc.bat
+++ proto/Kigyofarm/src/doc.bat
@@ -1,2 +1,2 @@
 set PATH=C:\Program Files\Java\jdk1.6.0_03\bin;%PATH%
-javadoc -d javadoc -nodeprecatedlist -doctitle "SPQR Snakefarm" levels/*.java snakefarm/*.java
+javadoc -d javadoc -nodeprecatedlist -doctitle "SPQR Snakefarm" snakefarm/*.java
