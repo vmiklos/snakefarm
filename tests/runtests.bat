2efe262a41be45b7f7b60ee67848148b55dacdb4
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-04-15 00:58:00 +0000

    runtests.bat frissitese a 18 testhez
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@210 dfce5827-3746-0410-9ec7-c6586dabde71

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -10,3 +10,63 @@ java -jar compare/TestCompare.jar test2.out curtest
 echo Running test 3...
 java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test3.in >curtest
 java -jar compare/TestCompare.jar test3.out curtest
+
+echo Running test 4...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test4.in >curtest
+java -jar compare/TestCompare.jar test4.out curtest
+
+echo Running test 5...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test5.in >curtest
+java -jar compare/TestCompare.jar test5.out curtest
+
+echo Running test 6...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test6.in >curtest
+java -jar compare/TestCompare.jar test6.out curtest
+
+echo Running test 7...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test7.in >curtest
+java -jar compare/TestCompare.jar test7.out curtest
+
+echo Running test 8...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test8.in >curtest
+java -jar compare/TestCompare.jar test8.out curtest
+
+echo Running test 9...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test9.in >curtest
+java -jar compare/TestCompare.jar test9.out curtest
+
+echo Running test 10...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test10.in >curtest
+java -jar compare/TestCompare.jar test10.out curtest
+
+echo Running test 11...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test11.in >curtest
+java -jar compare/TestCompare.jar test11.out curtest
+
+echo Running test 12...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test12.in >curtest
+java -jar compare/TestCompare.jar test12.out curtest
+
+echo Running test 13...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test13.in >curtest
+java -jar compare/TestCompare.jar test13.out curtest
+
+echo Running test 14...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test14.in >curtest
+java -jar compare/TestCompare.jar test14.out curtest
+
+echo Running test 15...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test15.in >curtest
+java -jar compare/TestCompare.jar test15.out curtest
+
+echo Running test 16...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test16.in >curtest
+java -jar compare/TestCompare.jar test16.out curtest
+
+echo Running test 17...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test17.in >curtest
+java -jar compare/TestCompare.jar test17.out curtest
+
+echo Running test 18...
+java -jar ../proto/Kigyofarm/dist/Kigyofarm.jar test18.in >curtest
+java -jar compare/TestCompare.jar test18.out curtest
