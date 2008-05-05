4187fd89d97fab75f6ba8c16859a22c6eb725fe8
vmiklos <vmiklos@dfce5827-3746-0410-9ec7-c6586dabde71>
2008-05-05 08:58:09 +0000

    ugyanez doc.*ra
    git-svn-id: http://spqr4.svn.sourceforge.net/svnroot/spqr4@255 dfce5827-3746-0410-9ec7-c6586dabde71

--- trunk/Kigyofarm/src/doc.bat
+++ trunk/Kigyofarm/src/doc.bat
@@ -1,2 +1,2 @@
 set PATH=C:\Program Files\Java\jdk1.6.0_03\bin;%PATH%
-javadoc -d javadoc -nodeprecatedlist -doctitle "SPQR Snakefarm" snakefarm/*.java
+javadoc -d javadoc -nodeprecatedlist -doctitle "SPQR Snakefarm" snakefarm/*.java snakefarm/creators/*.java snakefarm/viewfactories/*.java snakefarm/views/*.java
--- trunk/Kigyofarm/src/doc.sh
+++ trunk/Kigyofarm/src/doc.sh
@@ -1,1 +1,1 @@
-javadoc -d javadoc -nodeprecatedlist -doctitle "SPQR Snakefarm" snakefarm/*.java
+javadoc -d javadoc -nodeprecatedlist -doctitle "SPQR Snakefarm" snakefarm/*.java snakefarm/creators/*.java snakefarm/viewfactories/*.java snakefarm/views/*.java
